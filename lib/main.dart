366acd65c4a0e0071ab8f82abebf7aa3ea3dff5a
Kansha Maulidya Shyfa <kanshalidya12@gmail.com>
2023-11-13 14:59:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cupertino"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Delete Item"),
+                  content: Text("Are you sure?"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("No"),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+          child: Text("Alert Dialog"),
+        ),
+      ),
+    );
+  }
+}
